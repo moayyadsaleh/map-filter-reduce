0ad78fb1b9f35d0fc79fc75a31dd6c48455b3238
moayyadsaleh <moayyad.al-azzam@umontana.edu>
2023-08-27 11:28:28 -0600

    Add the functions discribtion for eaach function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+var numbers = [3, 56, 2, 48, 5];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//Map -Create a new array by doing something with each item in an array.
+const newNumbers = numbers.map(function (x) {
+  return x * 2;
+});
+console.log(newNumbers);
+//Filter - Create a new array by keeping the items that return true.
+const newNumbersFiltered = numbers.filter(function (num) {
+  return num > 10;
+});
+console.log(newNumbersFiltered);
+//Reduce - Accumulate a value by doing something to each item in an array.
+var newReducedNumbers = numbers.reduce(function (accumulator, currentNumber) {
+  return (accumulator += currentNumber);
+});
+console.log(newReducedNumbers);
+//Find - find the first item that matches from an array.
+const foundNumber = numbers.find(function (num) {
+  return num > 10;
+});
+console.log(foundNumber);
+//FindIndex - find the index of the first item that matches.
+const foundNumberIndex = numbers.findIndex(function (num) {
+  return num > 10;
+});
+
+console.log(foundNumberIndex);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render();
